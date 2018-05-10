e7dec1eec87f9a5211ca9a408d12226cc18ce7ab
AleksaVucetic <vucheta1@gmail.com>
2018-05-10 23:59:23 +0200

    Dodata klasa RoditeljGUINastavak u paketu ednevnik.gui

---
+++ ElektronskiDnevnik/src/ednevnik/gui/RoditeljGUINastavak.java
@@ -0,0 +1,241 @@
+package ednevnik.gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
+import java.awt.Dimension;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class RoditeljGUINastavak extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
+	private JTextField textField_8;
+	private JTextField textField_9;
+	private JTextField textField_10;
+	private JTextField textField_11;
+	private JTextField textField_12;
+	private JTextField textField_13;
+	private JTextField textField_14;
+	private JTextField textField_15;
+	private JTextField textField_16;
+	private JTextField textField_17;
+	private JTextField textField_18;
+	private JTextField textField_19;
+	private JTextField textField_20;
+	private JTextField textField_21;
+	private JTextField textField_22;
+	private JTextField textField_23;
+	private JTextField textField_24;
+	private JTextField textField_25;
+	private JTextField textField_26;
+	private JTextField textField_27;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					RoditeljGUINastavak frame = new RoditeljGUINastavak();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public RoditeljGUINastavak() {
+		setTitle("Roditelj / ucenik");
+		setResizable(false);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(RoditeljGUINastavak.class.getResource("/icons/Group-icon.png")));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 585, 493);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setEditable(false);
+		textField.setBounds(10, 11, 134, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setEditable(false);
+		textField_1.setBounds(10, 42, 134, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(154, 11, 146, 20);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(154, 42, 146, 20);
+		contentPane.add(textField_3);
+		textField_3.setColumns(10);
+		
+		textField_4 = new JTextField();
+		textField_4.setEditable(false);
+		textField_4.setBounds(10, 73, 134, 20);
+		contentPane.add(textField_4);
+		textField_4.setColumns(10);
+		
+		textField_5 = new JTextField();
+		textField_5.setEditable(false);
+		textField_5.setBounds(10, 104, 134, 20);
+		contentPane.add(textField_5);
+		textField_5.setColumns(10);
+		
+		textField_6 = new JTextField();
+		textField_6.setEditable(false);
+		textField_6.setBounds(10, 135, 134, 20);
+		contentPane.add(textField_6);
+		textField_6.setColumns(10);
+		
+		textField_7 = new JTextField();
+		textField_7.setEditable(false);
+		textField_7.setBounds(10, 166, 134, 20);
+		contentPane.add(textField_7);
+		textField_7.setColumns(10);
+		
+		textField_8 = new JTextField();
+		textField_8.setEditable(false);
+		textField_8.setBounds(10, 197, 134, 20);
+		contentPane.add(textField_8);
+		textField_8.setColumns(10);
+		
+		textField_9 = new JTextField();
+		textField_9.setEditable(false);
+		textField_9.setBounds(10, 228, 134, 20);
+		contentPane.add(textField_9);
+		textField_9.setColumns(10);
+		
+		textField_10 = new JTextField();
+		textField_10.setEditable(false);
+		textField_10.setBounds(10, 259, 134, 20);
+		contentPane.add(textField_10);
+		textField_10.setColumns(10);
+		
+		textField_11 = new JTextField();
+		textField_11.setEditable(false);
+		textField_11.setBounds(10, 290, 134, 20);
+		contentPane.add(textField_11);
+		textField_11.setColumns(10);
+		
+		textField_12 = new JTextField();
+		textField_12.setEditable(false);
+		textField_12.setBounds(10, 321, 134, 20);
+		contentPane.add(textField_12);
+		textField_12.setColumns(10);
+		
+		textField_13 = new JTextField();
+		textField_13.setEditable(false);
+		textField_13.setBounds(10, 352, 134, 20);
+		contentPane.add(textField_13);
+		textField_13.setColumns(10);
+		
+		textField_14 = new JTextField();
+		textField_14.setEditable(false);
+		textField_14.setBounds(10, 383, 134, 20);
+		contentPane.add(textField_14);
+		textField_14.setColumns(10);
+		
+		textField_15 = new JTextField();
+		textField_15.setColumns(10);
+		textField_15.setBounds(154, 73, 146, 20);
+		contentPane.add(textField_15);
+		
+		textField_16 = new JTextField();
+		textField_16.setColumns(10);
+		textField_16.setBounds(154, 104, 146, 20);
+		contentPane.add(textField_16);
+		
+		textField_17 = new JTextField();
+		textField_17.setColumns(10);
+		textField_17.setBounds(154, 135, 146, 20);
+		contentPane.add(textField_17);
+		
+		textField_18 = new JTextField();
+		textField_18.setColumns(10);
+		textField_18.setBounds(154, 166, 146, 20);
+		contentPane.add(textField_18);
+		
+		textField_19 = new JTextField();
+		textField_19.setColumns(10);
+		textField_19.setBounds(154, 197, 146, 20);
+		contentPane.add(textField_19);
+		
+		textField_20 = new JTextField();
+		textField_20.setColumns(10);
+		textField_20.setBounds(154, 228, 146, 20);
+		contentPane.add(textField_20);
+		
+		textField_21 = new JTextField();
+		textField_21.setColumns(10);
+		textField_21.setBounds(154, 259, 146, 20);
+		contentPane.add(textField_21);
+		
+		textField_22 = new JTextField();
+		textField_22.setColumns(10);
+		textField_22.setBounds(154, 290, 146, 20);
+		contentPane.add(textField_22);
+		
+		textField_23 = new JTextField();
+		textField_23.setColumns(10);
+		textField_23.setBounds(154, 321, 146, 20);
+		contentPane.add(textField_23);
+		
+		textField_24 = new JTextField();
+		textField_24.setColumns(10);
+		textField_24.setBounds(154, 352, 146, 20);
+		contentPane.add(textField_24);
+		
+		textField_25 = new JTextField();
+		textField_25.setColumns(10);
+		textField_25.setBounds(154, 383, 146, 20);
+		contentPane.add(textField_25);
+		
+		JLabel lblOpravdaniIzostanci = new JLabel("Opravdani izostanci:");
+		lblOpravdaniIzostanci.setFont(new Font("Open Sans", Font.PLAIN, 12));
+		lblOpravdaniIzostanci.setBounds(10, 421, 266, 14);
+		contentPane.add(lblOpravdaniIzostanci);
+		
+		JLabel lblNeopravdaniIzostanci = new JLabel("Neopravdani izostanci:");
+		lblNeopravdaniIzostanci.setFont(new Font("Open Sans", Font.PLAIN, 12));
+		lblNeopravdaniIzostanci.setBounds(10, 446, 266, 14);
+		contentPane.add(lblNeopravdaniIzostanci);
+		
+		textField_26 = new JTextField();
+		textField_26.setBounds(241, 421, 58, 14);
+		contentPane.add(textField_26);
+		textField_26.setColumns(10);
+		
+		textField_27 = new JTextField();
+		textField_27.setColumns(10);
+		textField_27.setBounds(241, 446, 58, 14);
+		contentPane.add(textField_27);
+	}
+
+}
